2dffd0ebec5e4ed41c40d981bfcc7e4a9edf54bc
William Rigby <WRigby1@humana.com>
2019-01-14 12:39:27 -0500

    Got rid of needless automapper constructor.

--- BusinessLayer/AutoMapperConfiguration.cs
+++ BusinessLayer/AutoMapperConfiguration.cs
@@ -17,17 +17,14 @@ namespace BusinessLayer
 
     public class AutoMapperProfiles : Profile
     {
-        public AutoMapperProfiles() : this("AutoMapperProfiles") { }
-
-        protected AutoMapperProfiles(string profileName) : base(profileName)
-        {
+        public AutoMapperProfiles() : base("AutoMapperProfiles") 
+        { 
             /* Entity -> BL Mappings */
-
             CreateMap<Entity.Models.Customer, BusinessLayer.Models.Customer>().ReverseMap();
             CreateMap<Entity.Models.Order, BusinessLayer.Models.Order>().ReverseMap();
             CreateMap<Entity.Models.OrderItem, BusinessLayer.Models.OrderItem>().ReverseMap();
             CreateMap<Entity.Models.Product, BusinessLayer.Models.Product>().ReverseMap();
-            CreateMap<Entity.Models.Supplier, BusinessLayer.Models.Supplier>().ReverseMap();            
+            CreateMap<Entity.Models.Supplier, BusinessLayer.Models.Supplier>().ReverseMap();     
         }
     }
 }
--- WebApi/AutoMapperConfiguration.cs
+++ WebApi/AutoMapperConfiguration.cs
@@ -21,18 +21,14 @@ namespace WebApi
 
     public class AutoMapperProfiles : Profile
     {
-        public AutoMapperProfiles() : this("AutoMapperProfiles") { }
-
-        protected AutoMapperProfiles(string profileName) : base(profileName)
-        {
+        public AutoMapperProfiles() : base("AutoMapperProfiles") 
+        { 
             /* BL -> Api Mappings */
-
             CreateMap<BusinessLayer.Models.Customer, WebApi.Models.Customer>().ReverseMap();
             CreateMap<BusinessLayer.Models.Order, WebApi.Models.Order>().ReverseMap();
             CreateMap<BusinessLayer.Models.OrderItem, WebApi.Models.OrderItem>().ReverseMap();
             CreateMap<BusinessLayer.Models.Product, WebApi.Models.Product>().ReverseMap();
             CreateMap<BusinessLayer.Models.Supplier, WebApi.Models.Supplier>().ReverseMap();
-
         }
     }
 }
